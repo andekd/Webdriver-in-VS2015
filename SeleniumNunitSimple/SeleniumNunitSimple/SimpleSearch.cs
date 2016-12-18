4dd2c9c2c25f9513a7df2bfecfcb3f1b9cfbe3b2
andekd <Anders.Ekdahl@cag.se>
2016-12-18 16:02:20 +0100

    First upload of everything

---
+++ SeleniumNunitSimple/SeleniumNunitSimple/SimpleSearch.cs
@@ -0,0 +1,48 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections;
+
+
+namespace SeleniumNunitSimple
+{
+    [TestFixture, Description("First NUnit run")]
+    public class SimpleSearch
+    {
+        //Empty Constructor
+        public SimpleSearch() { }
+
+        // Classmember to hold values to test for
+        private ArrayList myList;
+
+        [SetUp]
+        public void loadTestData()
+        {
+            myList = new ArrayList();
+            myList.Add("Foo");
+            myList.Add("Bar");
+            Console.WriteLine("Starting test");
+        }
+
+        [Test]
+        public void checkSize()
+        {
+            Assert.IsTrue(myList.Count == 2);
+        }
+
+        [TestCase("Foo")]
+        public void checkString(string toCheckFor)
+        {
+            bool exists = myList.Contains(toCheckFor);
+            Assert.IsTrue(exists);
+        }
+
+        [TearDown]
+        public void checkTearDown()
+        {
+            Assert.IsNotNull(myList);
+            myList = null;
+            Assert.IsNull(myList);
+        }
+
+    }
+}
