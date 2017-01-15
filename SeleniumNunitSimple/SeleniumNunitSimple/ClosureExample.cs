0151cd0e73543dc087489891ae8b5c9641d8c558
andekd <Anders.Ekdahl@cag.se>
2017-01-15 18:36:39 +0100

    added help-classes

---
+++ SeleniumNunitSimple/SeleniumNunitSimple/ClosureExample.cs
@@ -0,0 +1,32 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeleniumNunitSimple
+{
+    [TestFixture, Description("Check out closure")]
+    class ClosureExample
+    {
+        [Test]
+        public void TestClosure()
+        {
+            var inc = GetAFunc();
+            Console.WriteLine(inc(5));
+            Console.WriteLine(inc(6));
+        }
+
+        public Func<int, int> GetAFunc()
+        {
+            var myVar = 1;
+            Func<int, int> inc = delegate (int var1) //a delegate is a type safe function pointer 
+            {
+                myVar = myVar + 1;
+                return var1 + myVar;
+            };
+            return inc;
+        }
+    }
+}
